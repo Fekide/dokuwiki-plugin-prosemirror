02480fb9884ecacbfdbb0d561ee8e744630f337a
Yannick Lang <layaxx@gmx.de>
2023-04-29 14:03:52 +0200

    add compile script

---
+++ compile.sh
@@ -0,0 +1,3 @@
+yarn install
+yarn build
+zip -r dokuwiki-plugin-prosemirror ./action ./conf ./images ./lang ./lib ./parser ./schema ./deleted.files ./helper.php ./plugin.info.txt ./ProsemirrorException.php ./renderer.php ./requirements.txt ./script.js ./style.less ./customMenu.less
